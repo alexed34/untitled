3000d9d89a9fe3192f2c0a3dd55dcbbc05dab7fc
Эдуард <alexed34@yandex.ru>
2019-10-31 17:19:37 +0400

    новые функции

--- eledia/видеофайлы/main.py
+++ eledia/видеофайлы/main.py
@@ -42,9 +42,11 @@ dict_points = dict_url()
 
 # записываем текстовый файл
 def write_txt(name, list):
-    name2 = f'D:\видео эледиа\{name}\{name}.txt'
-    with open(name2, 'a') as f:
+    name2 = f'D:\\видео эледиа\\{name}\\{name}.txt'
+    with open(name2, 'w') as f:
         f.write(f'{name}\n')
+        f.write(f'{name} лечение с помощью акупунктурных точек \n')
+        f.write(f'акупунктура,эледиа,эледия,электроакупунктура \n')
         for i in list:
             f.write(f'{i:{5}}- {dict_points[i]}\n')
 
@@ -55,7 +57,6 @@ def write_txt_mpeg(name, list):
         f.write(f"file 'zastavka.mp4'\n")
         for i in list:
             f.write(f"file '{i}.mp4'\n")
-
 
 
 def copy_video(name, list):
@@ -71,41 +72,48 @@ def copy_video(name, list):
 
 
 def remove_file(name):
-    #os.rename('a.txt', 'b.kml')
+    # os.rename('a.txt', 'b.kml')
     path1 = f'D:\видео эледиа\{name}\{name}.mp4'
     path2 = f'D:\видео эледиа\{name}\zastavka.mp4'
     os.rename(path1, path2)
 
-def ffmpeg_file(name):
 
+def ffmpeg_file(name):
     os.chdir(f'D:\видео эледиа\{name}')
     os.system('ffmpeg -f concat -i mpeg2.txt -c copy all.mp4')
     time.sleep(10)
 
-
-
+def rider_img_ffmpeg(name, counter):
+    os.chdir(f'D:\видео эледиа\imgg')
+    os.system(f'ffmpeg -i input.png -vf "drawtext=font=arial:text={name}:\
+        fontcolor=0x000000:fontsize=60:x=50:y=200:" output{counter}.png ')
 
 def main():
-    with open('list_led_poln.csv') as f:
+    with open('list_led_poln.csv', ) as f:
         rider = csv.reader(f)
         counter = 0
         for i in rider:
             name = i[1].strip('"')  # название болезни
+            # name = name.encode('utf-8')
+            #
+            # name = name.decode('utf-8')
             print(name)
             #list = greate_list_points(i[2])  # список с точками болезни
             # for point in list:
             #     #print(point)
             #     if not os.path.exists(f'D:\видео эледиа\points\{point}.mp4'):
             #         print(f'{point}')
-            #writ_dir(name)  # создаем папку с именем болезни
-            #print(name, list)
-            #write_txt_mpeg(name, list)  # создаем txt файл с точками в папке с болезнью
-            #copy_video(name, list)
-            #remove_file(name)
-            #break
-            ffmpeg_file(name)
-            # counter += 1
-            # if counter >= 2:
+            # writ_dir(name)  # создаем папку с именем болезни
+            # print(name, list)
+            # write_txt_mpeg(name, list)  # создаем txt файл с точками в папке с болезнью
+            # copy_video(name, list)
+            # remove_file(name)
+            # write_txt(name, list)
+            # break
+            #ffmpeg_file(name)
+            rider_img_ffmpeg(name, counter)
+            counter += 1
+            # if counter >= 1:
             #     break
 
 
